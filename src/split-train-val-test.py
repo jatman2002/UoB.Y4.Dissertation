feee766f2d5e45434fd761726990e56586796553
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-03-10 13:38:17 +0000

    create train-test split

---
+++ src/split-train-val-test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+for i in range(1,6):
+    bookings = pd.read_csv(f'C:/git/UoB.Y4.Dissertation/src/SQL-DATA/Restaurant-{i}-bookings.csv')
+
+    dates = pd.to_datetime(bookings['BookingDate'])
+    unique_days = dates.dt.date.unique()
+    train_idx = int(len(unique_days) * 0.85)
+    # val_idx = int(len(unique_days) * 0.85)
+
+    train_days, test_days = unique_days[:train_idx], unique_days[train_idx:]
+
+
+    train_data = bookings[dates.dt.date.isin(train_days)]
+    # val_data = bookings[dates.dt.date.isin(val_days)]
+    test_data = bookings[dates.dt.date.isin(test_days)]
+
+    train_data.to_csv(f'C:/git/UoB.Y4.Dissertation/src/SQL-DATA/Restaurant-{i}-train.csv', sep=',', encoding='utf-8', index=False)
+    # val_data.to_csv(f'C:/git/UoB.Y4.Dissertation/src/SQL-DATA/Restaurant-{i}-val.csv', sep=',', encoding='utf-8', index=False)
+    test_data.to_csv(f'C:/git/UoB.Y4.Dissertation/src/SQL-DATA/Restaurant-{i}-test.csv', sep=',', encoding='utf-8', index=False)
+
+    print(f'{i}\t{len(train_data)=}\t{len(test_data)=}')

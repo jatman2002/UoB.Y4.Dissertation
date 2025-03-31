01c5a6a0c31f0f3256ec6baecf0ead24cd6b02b8
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-03-31 16:01:46 +0100

    put testing back in

--- src/RL/DQN.py
+++ src/RL/DQN.py
@@ -261,10 +261,10 @@ print()
 
 torch.save(policy_network.state_dict(), f'{os.getcwd()}/models/DQN3.pt')
 
-# test_data = pd.read_csv(f'{os.getcwd()}/src/SQL-DATA/Restaurant-{restaurant_name}-test.csv')
-# test_data['BookingStartTime'] = (test_data['BookingStartTime'] - 36000) / (60*15)
-# test_data['Duration'] = test_data['Duration'] / (60*15)
-# test_data["EndTime"] = test_data["BookingStartTime"] + test_data["Duration"]
+test_data = pd.read_csv(f'{os.getcwd()}/src/SQL-DATA/Restaurant-{restaurant_name}-test.csv')
+test_data['BookingStartTime'] = (test_data['BookingStartTime'] - 36000) / (60*15)
+test_data['Duration'] = test_data['Duration'] / (60*15)
+test_data["EndTime"] = test_data["BookingStartTime"] + test_data["Duration"]
 
-# print()
-# test_predictor(f'Restaurant-{restaurant_name}/DQN3', test_data, tables, policy_network, find_table, device, features)
+print()
+test_predictor(f'Restaurant-{restaurant_name}/DQN3', test_data, tables, policy_network, find_table, device, features)

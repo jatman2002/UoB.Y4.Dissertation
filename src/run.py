aaf9fccdba06e46f9df9be75c19a8b1f475916f2
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-06 18:09:43 +0100

    get all ppo runs done

--- src/run.py
+++ src/run.py
@@ -31,9 +31,9 @@ sys.stdout = LoggerWriter()
 
 gpu = 5
 print(f'Restaurant {2}\tDQN', file=original_stdout)
-DQN(2, gpu).run()
+# DQN(2, gpu).run()
 for r in range(3,6):
     print(f'Restaurant {r}\tPPO', file=original_stdout)
     PPO(r, gpu).run()
-    print(f'Restaurant {r}\tDQN', file=original_stdout)
-    DQN(r, gpu).run()
+    # print(f'Restaurant {r}\tDQN', file=original_stdout)
+    # DQN(r, gpu).run()

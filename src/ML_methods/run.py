6e9c2953b193462cf19cc7858ba76b747672605f
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-06 14:45:03 +0100

    ran the wrong one

--- src/ML_methods/run.py
+++ src/ML_methods/run.py
@@ -1,8 +1,8 @@
 # import LR
 import MLP1 as MLP1
-import MLP2 as MLP2
+# import MLP2 as MLP2
 import MLP3 as MLP3
-import MLP4 as MLP4
+# import MLP4 as MLP4
 # import OVR
 # import RF
 # import GridSearch.RFGridSearch as RFGridSearch
@@ -13,10 +13,10 @@ import helper.Results as Results
 restaurant_name = '1'
 
 
-# MLP1.run(restaurant_name)
+MLP1.run(restaurant_name)
 # MLP2.run(restaurant_name)
-# MLP3.run(restaurant_name)
-MLP4.run(restaurant_name)
+MLP3.run(restaurant_name)
+# MLP4.run(restaurant_name)
 
 Results.run(restaurant_name, 'MLP1')
 # Results.run(restaurant_name, 'MLP2')

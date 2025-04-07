cc319afd339f6f01de5c000e7edf651c4369136b
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-07 22:24:57 +0100

    undo sh

--- run.sh
+++ run.sh
@@ -1,26 +1,26 @@
 #! /bin/bash
 
-python3 app/src/train.py -r 1 -a MLP1
-python3 app/src/train.py -r 1 -a MLP2
-python3 app/src/train.py -r 1 -a MLP3
-python3 app/src/train.py -r 1 -a MLP4
+python3 src/train.py -r 1 -a MLP1
+python3 src/train.py -r 1 -a MLP2
+python3 src/train.py -r 1 -a MLP3
+python3 src/train.py -r 1 -a MLP4
 
-python3 app/src/train.py -r 2 -a MLP1
-python3 app/src/train.py -r 2 -a MLP2
-python3 app/src/train.py -r 2 -a MLP3
-python3 app/src/train.py -r 2 -a MLP4
+python3 src/train.py -r 2 -a MLP1
+python3 src/train.py -r 2 -a MLP2
+python3 src/train.py -r 2 -a MLP3
+python3 src/train.py -r 2 -a MLP4
 
-python3 app/src/train.py -r 3 -a MLP1
-python3 app/src/train.py -r 3 -a MLP2
-python3 app/src/train.py -r 3 -a MLP3
-python3 app/src/train.py -r 3 -a MLP4
+python3 src/train.py -r 3 -a MLP1
+python3 src/train.py -r 3 -a MLP2
+python3 src/train.py -r 3 -a MLP3
+python3 src/train.py -r 3 -a MLP4
 
-python3 app/src/train.py -r 4 -a MLP1
-python3 app/src/train.py -r 4 -a MLP2
-python3 app/src/train.py -r 4 -a MLP3
-python3 app/src/train.py -r 4 -a MLP4
+python3 src/train.py -r 4 -a MLP1
+python3 src/train.py -r 4 -a MLP2
+python3 src/train.py -r 4 -a MLP3
+python3 src/train.py -r 4 -a MLP4
 
-python3 app/src/train.py -r 5 -a MLP1
-python3 app/src/train.py -r 5 -a MLP2
-python3 app/src/train.py -r 5 -a MLP3
-python3 app/src/train.py -r 5 -a MLP4
+python3 src/train.py -r 5 -a MLP1
+python3 src/train.py -r 5 -a MLP2
+python3 src/train.py -r 5 -a MLP3
+python3 src/train.py -r 5 -a MLP4

84ba41ed6ec0b98bc65c378d73a401a840d1cf13
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-09 15:12:38 +0100

    update run.sh to retrain mlp4

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
 
-export CUDA_VISIBLE_DEVICES=-1
-
-python3 src/ModelTesting/run.py -r 1 -a MLP1 -v -R
-python3 src/ModelTesting/run.py -r 1 -a MLP2 -v -R
-python3 src/ModelTesting/run.py -r 1 -a MLP3 -v -R
-python3 src/ModelTesting/run.py -r 1 -a MLP4 -v -R
+CUDA_VISIBLE_DEVICES=2 python3 src/train.py -r 1 -a MLP4
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP4 -v -R

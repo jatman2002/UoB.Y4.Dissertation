312fe59667795de60515d3c5597d3e615e3330b3
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-12 14:35:45 +0100

    run

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@
 # CUDA_VISIBLE_DEVICES=3 python3 src/train.py -r 3 -a MLP3
 # CUDA_VISIBLE_DEVICES=3 python3 src/train.py -r 4 -a MLP4
 
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP1 -R -v
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP2 -R -v
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP3 -R -v
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP4 -R -v
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP1 -R -v
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP2 -R -v
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP3 -R -v
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP4 -R -v
 
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a MLP -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a MLP -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a MLP -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a MLP -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a MLP -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a MLP -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a MLP -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a MLP -R
 
 # CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 1 -a RF
 # CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 2 -a RF

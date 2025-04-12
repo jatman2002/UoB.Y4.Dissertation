28a609bdfa4a8650892d66cd5600ad66945f2065
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-12 15:44:15 +0100

    one last run?

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@
 # CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP3 -R -v
 # CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP4 -R -v
 
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a MLP -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a MLP -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a MLP -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a MLP -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a MLP -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a MLP -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a MLP -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a MLP -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a MLP -R
 
 # CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 1 -a RF
 # CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 2 -a RF
@@ -34,8 +34,8 @@ CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a MLP -R
 # CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 4 -a LR
 # CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 5 -a LR
 
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a LR -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a LR -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a LR -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a LR -R
-# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a LR -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a LR -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a LR -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a LR -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a LR -R
+CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a LR -R

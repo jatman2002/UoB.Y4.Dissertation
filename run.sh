7d7d595bbfba099b6e3eceaef3ac20b417c9acc0
Jatin Yadav <jatin.yadav@favouritetable.com>
2025-04-12 13:58:37 +0100

    update run

--- run.sh
+++ run.sh
@@ -24,14 +24,14 @@ CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a RF -R
 CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a RF -R
 CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a RF -R
 
-CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 1 -a LR
-CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 2 -a LR
-CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 3 -a LR
-CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 4 -a LR
-CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 5 -a LR
+# CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 1 -a LR
+# CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 2 -a LR
+# CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 3 -a LR
+# CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 4 -a LR
+# CUDA_VISIBLE_DEVICES=-1 python3 src/train.py -r 5 -a LR
 
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a LR -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a LR -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a LR -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a LR -R
-CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a LR -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 1 -a LR -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 2 -a LR -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 3 -a LR -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 4 -a LR -R
+# CUDA_VISIBLE_DEVICES=-1 python3 src/ModelTesting/run.py -r 5 -a LR -R
